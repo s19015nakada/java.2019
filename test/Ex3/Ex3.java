4b65923a413ea7a89b3679131ab559fe3fd7b0cb
ryusei <s19015@std.it-college.ac.jp>
2019-11-28 10:20:05 +0900

    test commit

--- test/Ex3/Ex3.java
+++ test/Ex3/Ex3.java
@@ -11,63 +11,145 @@ class Ex3{
 	}
 
 	public int ex3_1(int n){
-		
-		return -1;
-
-	}
+	if (n > 0){
+          return n;
+    }else if (n < 0){
+           return -n;
+    } else{
+      return 0;
+    }
+  }
 
 	public String ex3_2(int a, int b){
-
-		return "dummy";
-	}
+    String tar = "";
+    if (a%b==0){
+       tar = "4は12の約数です";
+       return tar;
+    }else {
+       tar = "5は12の約数ではありません";
+       return tar;
+    }
+   }
 
 	public String ex3_3(int a, int b){
-
-		return "dummy";
-
-	}
+    if (a > b){
+       return "8の方が大きいです";
+    }else if (a < b){
+       return "9の方が大きいです";
+    }else {
+       return  "同じ値です";
+    }
+   }
 
 	public String ex3_4(int a){
-
-		return "dummy";
-
-	}
+    if (0 < a && a % 5 == 0){
+       return "その値は5で割り切れます";
+    }else if (0 > a){
+       return "正でない整数値です";
+    }else{
+       return "その値は5で割り切れません";
+    }
+   }
 
 	public String ex3_5(int a){
+    if (a % 10 == 0){
+       return "その値は10の倍数です";
+    }else if (0 > a){
+       return "正でない整数値です";
+    }else{
+       return "その値は10の倍数ではありません";
+    }
+   }
 
-		return "dummy";
-	}
-	
 	public String ex3_6(int a){
-		return "dummy";
-	}
+    if (0 < a && a % 3 == 0){
+       return "その値は3で割り切れます";
+    }else if (0 < a && a % 3 == 1){
+       return "その値を3で割った余りは1です";
+    }else if (0 < a && a % 3 == 2){
+       return "その値を3で割った余りは2です";
+    }else{
+       return  "正でない整数値です";
+    }
+   }
 
 	public String ex3_7(int a){
-		return "dummy";
-	}
+    if (0 <= a && a <= 59){
+       return "不可";
+    }else if (60 <= a && a <= 69){
+       return "可";
+    }else if (70 <= a && a <= 79){
+       return "良";
+    }else if (80 <= a && a <= 100){
+       return "優";
+    }else {
+       return "範囲(0～100)外の値です";
+    }
+   }
 
 	public double ex3_8(double a, double b){
-		return -1.0;
-	}
+    if (a > b){
+       return a;
+    }else if (a < b){
+       return b;
+    }else{
+       return 0;
+    }
+   }
 
 	public int ex3_9(int a, int b){
-		return 0;
-	}
+        return a-b;
+    }
 
 	public String ex3_10(int a, int b){
-		return "dummy";
-	}
+    int c = a-b;
+    if (c <= 10){
+       return "それらの差は10以下です";
+    }else {
+       return "それらの差は11以上です";
+    }
+   }
 
 	public int ex3_11(int a, int b, int c){
-		return 1000;
-	}
+    if (a <= b && a <= c){
+       return a;
+    }else if (b <= a && b <= c){
+       return b;
+    }else {
+       return c;
+    }
+   }
 
 	public int ex3_12(int a, int b, int c){
-		return 1000;
-	}
+    if (a >= b) {
+            if (b >= c) {
+                return b;
+            } else if (a <= c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else if (a > c) {
+            return a;
+        } else if (b > c) {
+            return c;
+        } else {
+            return b;
+        }
+    }
 
 	public String ex3_13(int a){
-		return "dummy";
-	}
+    if (a> 0 &&(a==12 || a<=2)){
+       return "冬";
+    }else if(a > 0 &&(3<=a && a<=5)){
+       return "春";
+    }else if (a > 0 &&(6<=a && a<=8)){
+       return "夏";
+    }else if (a > 0 &&(9<=a && a<=11)){
+       return "秋";
+    }else {
+       return "範囲(1～12)外の値です";
+    }
+   }
 }
 
--- test/Ex4/Ex4.java
+++ test/Ex4/Ex4.java
@@ -1,66 +1,117 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 class Ex4{
 
 	public int [] ex4_1(int [] a){
-		
-		return a;
-
+		int[] t = new int[a.length];
+      for( int i = 0; i < t.length; i++ ){
+				t[i] = a[i] ;
+			}
+				Arrays.sort(t);
+		return t;
 	}
 
 	public int [] ex4_2(int [] a){
-		
-		return a;
-
+		int[] t = new int[a.length];
+		int[] t2 = new int[a.length];
+    for( int i = 0; i < t2.length; i++ ){
+			t2[i] = a[i] ;
+		}
+		Arrays.sort(t2);
+		for( int i = 0; i < t2.length; i++ ){
+			t[i] = t2[t2.length - 1 -i] ;
+		}
+		return t;
 	}
 
 	public String [] ex4_3(int [] a){
-		
-		String [] hantei = {"dummy"};
-		
-		return hantei;
+		String[] t = new String[a.length];
+		for (int i = 0; i < a.length ;i++ ) {
+			int A = a[i];
+			if (A < 0) {
+				t[i] = "負";
+			}
+			else if (A == 0) {
+				t[i] = "零";
+			}
+			else {
+				t[i] = "正";
+			}
+		}
+
+		return t;
 
 	}
 
 	public int ex4_4(int a){
-		
-		return -1;
+		int A = String.valueOf(a).length();
+
+		return A;
 
 	}
 
 	public int ex4_5(int a){
-		
-		return -1;
+		int A = 0;
+		for (int i = 1;i<=a ;i++ ) {
+			A += i;
+		}
+
+		return A;
 
 	}
 
 	public int [] ex4_6(int a){
-		
-		int [] ret = new int[1];
-		
+
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 1;i<=a ;i++) {
+			if (a%i == 0) {
+				list.add(i);
+			}
+		}
+		int[] ret = new int[list.size()];
+		for (int i = 0; i< list.size() ;i++) {
+			ret[i] = list.get(i);
+		}
 		return ret;
 
 	}
 
 	public boolean ex4_7(int a){
-		
-		return false;
-
+		boolean RE = true;
+		if (a == 1) {
+			RE = false;
+		}
+		else {
+			for (int i = 2; i < a ;i++ ) {
+				if (a % i == 0 ) {
+					RE = false;
+				}
+			}
+		}
+		return RE;
 	}
 
 	public int ex4_8(int [] a){
-		
 		int ret = 0;
-		
+		for (int i = 0; i < a.length ;i++ ) {
+			ret += a[i];
+		}
+
 		return ret;
 
 	}
 
 	public int ex4_9(int [] a){
-		
-		int ret = 0;
-		
-		return ret;
+		double ret = 0;
+		for (int i = 0; i < a.length ;i++ ) {
+			ret += a[i];
+		}
+		int i =  (int) (Math.round(ret /=a.length));
+
+		return i;
 
 	}
 }
